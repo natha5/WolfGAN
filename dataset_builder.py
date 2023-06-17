8df9c091ffc92377a2d89db13f48029755842a28
natha5 <ns539@canterbury.ac.uk>
2023-06-17 22:44:00 +0100

    <add> Pnadas import

--- dataset_builder.py
+++ dataset_builder.py
@@ -1,5 +1,18 @@
 import numpy as np
+import pandas as pd
 
 current_file = open(r"map_dataset\base_game\LEVEL01.HEX","r")
 
-print(current_file.read())
+current_maze = current_file.readlines()
+
+# now, put the maze data into a list
+
+
+maze = np.zeros((64,64), str)
+
+for i in range(64):
+    for j in range(64):
+        for k in range(0,128,2):
+            maze[i][j] = str(current_maze[i + 5])[k:k+2]
+
+print(maze)
