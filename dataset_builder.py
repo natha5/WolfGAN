c1b8501cf7bb638519d7081b4ed152cf2c20a5bd
natha5 <ns539@canterbury.ac.uk>
2023-06-18 23:12:20 +0100

    Update dataset_builder.py

--- dataset_builder.py
+++ dataset_builder.py
@@ -1,18 +1,34 @@
 import numpy as np
 import pandas as pd
 
-current_file = open(r"map_dataset\base_game\LEVEL01.HEX","r")
 
-current_maze = current_file.readlines()
+def read_into_array():
 
-# now, put the maze data into a list
+    maze_array = [["0"] * 64 for _ in range(64)]  # Initialize a 64x64 array with zeros
+    object_array = [["0"] * 64 for _ in range(64)]
 
 
-maze = np.zeros((64,64), str)
+    with open(r"map_dataset\base_game\LEVEL01.HEX", 'r') as file:
 
-for i in range(64):
-    for j in range(64):
-        for k in range(0,128,2):
-            maze[i][j] = str(current_maze[i + 5])[k:k+2]
+        for _ in range(5): # skip first 5 lines
+            file.readline()
 
-print(maze)
+        for i in range(64):
+            line = file.read(128)  # Read 128 characters (64 values) at a time
+            for j in range(64):
+                value = line[j*2 : j*2+2]  # Extract the 2-character value
+                maze_array[i][j] = str(value)
+            file.readline()  # Skip the remaining characters in the line (if any)
+    
+        for _ in range(3): # skip 3 more lines
+            file.readline()
+
+        for i in range(64):
+            line = file.read(128)  # Read 128 characters (64 values) at a time
+            for j in range(64):
+                value = line[j*2 : j*2+2]  # Extract the 2-character value
+                object_array[i][j] = str(value)
+            file.readline()  # Skip the remaining characters in the line (if any)
+
+    return(maze_array,object_array)
+
