f541c3cde6291dd55e6a470bbce2c16caea55379
natha5 <ns539@canterbury.ac.uk>
2023-07-04 10:23:35 +0100

    <add>dataset.csv, writing values of maps to csv now works

--- dataset_builder.py
+++ dataset_builder.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
+import csv
 
 
 def read_into_array(file_path):
 
-    maze_array = [["0"] * 64 for _ in range(64)]  # Initialize a 64x64 array with zeros
-    object_array = [["0"] * 64 for _ in range(64)]
+    maze_array = [[0] * 64 for _ in range(64)]  # Initialize a 64x64 array with zeros
+    #object_array = [["0"] * 64 for _ in range(64)]
 
 
     with open(file_path, 'r') as file:
@@ -17,9 +18,10 @@ def read_into_array(file_path):
             line = file.read(128)  # Read 128 characters (64 values) at a time
             for j in range(64):
                 value = line[j*2 : j*2+2]  # Extract the 2-character value
-                maze_array[i][j] = str(value)
+                maze_array[i][j] = hex_string_to_int(str(value))
             file.readline()  # Skip the remaining characters in the line (if any)
-    
+
+        """
         for _ in range(3): # skip 3 more lines
             file.readline()
 
@@ -29,13 +31,13 @@ def read_into_array(file_path):
                 value = line[j*2 : j*2+2]  # Extract the 2-character value
                 object_array[i][j] = str(value)
             file.readline()  # Skip the remaining characters in the line (if any)
-
-    return(maze_array,object_array)
+        """
+    return(maze_array)
 
 
 def determine_path(base_path):
     wall_list = []
-    objects_list = []
+    #objects_list = []
 
     for i in range(1,60):
         if i < 10:
@@ -43,29 +45,38 @@ def determine_path(base_path):
         else:
             current_path = base_path + r"\LEVEL" + str(i) +".HEX"
         
-        walls, objects = read_into_array(current_path)
+        print("Map #" + str(i))
+
+        walls = read_into_array(current_path)
         wall_list.append(walls)
-        objects_list.append(objects)
+        #objects_list.append(objects)
     
-    return (wall_list, objects_list)
+    return (wall_list)
         
+
+def hex_string_to_int(value):
+    decimal_value = int(value, 16)
+    return decimal_value
 
 
 
 
 full_walls_list = []
-full_objects_list = []
+#full_objects_list = []
 
+"""
 walls, objects = determine_path(r"map_dataset\10newones")
 
 full_walls_list.append(walls)
 full_objects_list.append(objects)
+"""
 
-walls, objects = determine_path(r"map_dataset\base_game")
+walls = determine_path(r"map_dataset\base_game")
 
 full_walls_list.append(walls)
-full_objects_list.append(objects)
+# full_objects_list.append(objects)
 
+"""
 walls, objects = determine_path(r"map_dataset\DHWTCSDL")
 
 full_walls_list.append(walls)
@@ -85,34 +96,19 @@ walls, objects = determine_path(r"map_dataset\W3D-CMP")
 
 full_walls_list.append(walls)
 full_objects_list.append(objects)
+"""
+
 
 full_walls_array = np.array(full_walls_list)
-full_objects_array = np.array(full_objects_list)
+# full_objects_array = np.array(full_objects_list)
 
 print(full_walls_array.shape)
 
-full_walls_array = np.reshape(full_walls_array,((59*6), (64*64)))
-full_objects_array = np.reshape(full_objects_array,((59*6), (64*64)))
 
+with open('dataset.csv', 'w') as f:
+    writer = csv.writer(f)
 
-df = pd.DataFrame(full_walls_array)
+    for i in range(59):
+        for j in range(64):
 
-df = pd.concat([df, pd.DataFrame(full_objects_array)], axis=1)
-
-# create column names
-
-column_names = []
-
-for i in range(64*64):
-    column_names.append("w"+ str(i+1))
-
-for i in range(64*64):
-    column_names.append("o" + str(i+1))
-
-
-df.columns = column_names
-
-print(df)
-
-
-
+            writer.writerow(full_walls_array[0][i][j])
